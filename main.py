a73a85d7cacbc79fe718239d3120dd0cdef7f6d2
Mateo Pomies <35891922+sauvajohnz@users.noreply.github.com>
2019-06-09 00:13:58 +0200

    correction d'un commentaire

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import os # On importe le module os
+import os # On importe le module os nécéssaire à la dernière ligne du programme
 
-#test
+i = 0 # On initialise les variables nécéssaires au programme
+table = input("Table de multiplication : ") #On demande a l'utilisateur de remplir les variables
+table_limit = input("Limite de nombre a afficher : ")
 
-print("coucou mon spiltercos")
-print("sale fils de pute")
-print("ta mère ta chier par le vagin")
-print("ok sa marche")
+table = int(table) # On transforme les variables que l'utilisateur a remplies en variables de type INT
+table_limit = int(table_limit)
 
-os.system("pause")
+
+while i < table_limit : # On calcule en affichant sur la console les résultats
+	print(table, "x", i+1, " = ",table*(i+1))
+	i+=1
+
+os.system("pause") # On met la console en pause à la fin du programme
