e37650cc652326c433e5c4b56ce9444214384b53
Mateo Pomies <35891922+sauvajohnz@users.noreply.github.com>
2019-06-09 01:20:55 +0200

    Suppresion main + tirets qui s'adaptent a la taille de la ligne dans le programme table

--- table_multiplication.py
+++ table_multiplication.py
@@ -1,4 +1,12 @@
 import os # On importe le module os
+import math 
+
+def tirets(nombre_tirets): #On crée une fonction qui une fois appellée affiche un certain nombre de tirets
+	nombre_tirets = int(nombre_tirets)
+	a = 0
+	while a < nombre_tirets:
+		print("-", end='')
+		a+=1
 
 i = 0 # On initialise les variables nécéssaires au programme
 table = input("Table de multiplication : ") #On demande a l'utilisateur de remplir les variables
@@ -7,9 +15,19 @@ table_limit = input("Limite de nombre a afficher : ")
 table = int(table) # On transforme les variables que l'utilisateur a remplies en variables de type INT
 table_limit = int(table_limit)
 
+table_longueur = math.log10(table) + 1 # On recupere la taille de la table
+
+
+
 
 while i < table_limit : # On calcule en affichant sur la console les résultats
-	print(table, "x", i+1, " = ",table*(i+1))
-	i+=1
+	table_limit_longueur = math.log10(i+1) + 1 # On recupere la taille du facteur
+	resultat_longueur = math.log10(table*(i+1)) + 1 # On recupere la taille du résultat
+	print("")
+	tirets(8+table_longueur+table_limit_longueur+resultat_longueur) # On adapte le nombre de tirets en fonction de la taille de la ligne
+	print("\n",table, "x", i+1, " =",table*(i+1)) # On affiche la table et son resultat
+	tirets(8+table_longueur+table_limit_longueur+resultat_longueur)
+	i+=1 
 
+print("\nFin du programme")
 os.system("pause") # On met la console en pause à la fin du programme
