7a831932094e28ef890b0ce04af050ac1ade9636
Sauvajohnz <mateopomies22@gmail.com>
2019-06-15 13:05:09 +0200

    ajout de la correction

---
+++ correction premier_programme.py
@@ -0,0 +1,20 @@
+import os # On importe une librairie nommee OS
+
+def traiter_x(x): # On creer une fonction qui traite un nombre selon un certain nombre d'operaitions
+	return (x+2)*2
+
+print("Le programme va vous demander de choisir en boucle des valeurs de x")
+print("Pour sortir, il suffira de choisir la valeur 0.\n\n")
+
+while(1==1):
+	nombre_utilisateur = int(input("Rentrer la valeur de x : ")) # On demande a l'utilisateur de rentrer un nombre dans la variable nombre_utilisateur
+	if nombre_utilisateur==0: # On regarde si l'utilisateur a mis le nombre 0
+		break;
+	elif nombre_utilisateur>0: # Ou si il a bien respecte la condition de mettre un nombre positif
+		print("f("+str(nombre_utilisateur)+") = "+ str(traiter_x(nombre_utilisateur))+ "\n") # On affiche le resultat
+	else: # Sinon on en redemande un
+		print("Il faut une valeur positive ou nulle !\n")
+
+print("\nFin du programme :)\n\n")
+
+os.system("pause") # On met le programme en pause a sa fin
--- table_multiplication.py
+++ table_multiplication.py
@@ -1,19 +1,18 @@
-# Esthetique 
 import os # On importe le module os
 import math 
 
-def tirets(nombre_tirets): #On crée une fonction qui une fois appellée affiche un certain nombre de tirets
+def tirets(nombre_tirets): 
 	nombre_tirets = int(nombre_tirets)
 	a = 0
 	while a < nombre_tirets:
 		print("-", end='')
 		a+=1
 
-i = 0 # On initialise les variables nécéssaires au programme
-table = input("Table de multiplication : ") #On demande a l'utilisateur de remplir les variables
+i = 0 # On initialise les variables necessaires au programme
+table = input("Table de multiplication : ") #On demande a l utilisateur de remplir les variables
 table_limit = input("Limite de nombre a afficher : ")
 
-table = int(table) # On transforme les variables que l'utilisateur a remplies en variables de type INT
+table = int(table) # On transforme les variables que l utilisateur a remplies en variables de type INT
 table_limit = int(table_limit)
 
 table_longueur = math.log10(table) + 1 # On recupere la taille de la table
@@ -21,9 +20,9 @@ table_longueur = math.log10(table) + 1 # On recupere la taille de la table
 
 
 
-while i < table_limit : # On calcule en affichant sur la console les résultats
+while i < table_limit : # On calcule en affichant sur la console les resultats
 	table_limit_longueur = math.log10(i+1) + 1 # On recupere la taille du facteur
-	resultat_longueur = math.log10(table*(i+1)) + 1 # On recupere la taille du résultat
+	resultat_longueur = math.log10(table*(i+1)) + 1 # On recupere la taille du resultat
 	print("")
 	tirets(8+table_longueur+table_limit_longueur+resultat_longueur) # On adapte le nombre de tirets en fonction de la taille de la ligne
 	print("\n",table, "x", i+1, " =",table*(i+1)) # On affiche la table et son resultat
@@ -31,4 +30,5 @@ while i < table_limit : # On calcule en affichant sur la console les résultats
 	i+=1 
 
 print("\nFin du programme")
-os.system("pause") # On met la console en pause à la fin du programme
+
+os.system("pause") # On met la console en pause a la fin du programme
