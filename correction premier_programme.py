eada7e2fcfcf45fd84556d43a55c02b03749537e
Sauvajohnz <mateopomies22@gmail.com>
2019-06-15 15:13:57 +0200

    bug quand l'utilisateur ne met pas un nombre fixé

--- correction premier_programme.py
+++ correction premier_programme.py
@@ -7,7 +7,15 @@ print("Le programme va vous demander de choisir en boucle des valeurs de x")
 print("Pour sortir, il suffira de choisir la valeur 0.\n\n")
 
 while(1==1):
-	nombre_utilisateur = int(input("Rentrer la valeur de x : ")) # On demande a l'utilisateur de rentrer un nombre dans la variable nombre_utilisateur
+	test_valeur = False
+	while test_valeur==False:
+		try:
+			nombre_utilisateur = int(input("Rentrer la valeur de x : ")) # On demande a l'utilisateur de rentrer un nombre dans la variable nombre_utilisateur
+			test_valeur = True
+		except ValueError:
+			print("Erreur dans la valeur de x\n")
+			test_valeur = False	
+
 	if nombre_utilisateur==0: # On regarde si l'utilisateur a mis le nombre 0
 		break;
 	elif nombre_utilisateur>0: # Ou si il a bien respecte la condition de mettre un nombre positif
